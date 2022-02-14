cfc949a72c8bdb46fd187dd8d207ab55192d1618
berkoooooooo <82767199+berkoooooooo@users.noreply.github.com>
2022-02-14 11:11:00 +0100

    Update autoUploader.js

--- dist/js/autoUploader.js
+++ dist/js/autoUploader.js
@@ -8,12 +8,12 @@ const setSavedData=(newData)=>localStorage.setItem(storageKey,JSON.stringify(Obj
 function sendNotification(placeId){if(!("Notification"in window)||Notification.permission!="granted"||!getSavedData().notify)return;const gamelink=getGameLink(placeId)
 const notif=new Notification("New condo posted!",{body:gamelink,icon:`https://www.roblox.com/asset-thumbnail/image?assetId=${placeId}&width=420&height=420&format=png`})
 notif.onclick=(event)=>{event.preventDefault();window.open(gamelink,"_blank")}}
-const condoCheck=(sendNotif=false)=>fetch(`/latestupload.json?cache=${Date.now()}`,{headers:{pragma:"no-cache","cache-control":"no-cache"}}).then((response)=>response.json()).then((json)=>{uploadMessage.innerHTML=json.success?"Condo has been found!":json.message
+const condoCheck=(sendNotif=false)=>fetch(`https://condogames.xyz/latestupload.json?cache=${Date.now()}`,{headers:{pragma:"no-cache","cache-control":"no-cache"}}).then((response)=>response.json()).then((json)=>{uploadMessage.innerHTML=json.success?"Condo has been found!":json.message
 if(currentPlaceId!=json.placeId){playButton.href=!isPhone?getGameLink(json.placeId):`robloxmobile://placeID=${json.placeId}`
 if(json.placeId&&sendNotif)sendNotification(json.placeId);}
 currentPlaceId=json.placeId
 playButton.hidden=!currentPlaceId})
 setInterval(()=>condoCheck(true),10000)
 condoCheck()
-if("Notification"in window&&Notification.permission=="default"&&getSavedData().notify!=false){setTimeout(()=>swal.fire({title:"Auto Uploads",text:"Do you want to receive notifications when a new condo gets uploaded?",icon:"question",showDenyButton:true,confirmButtonText:'Sure',denyButtonText:'I HATE CONDOS',}).then((result)=>{if(result.isConfirmed){Notification.requestPermission().then((permission)=>{if(permission!="granted")return;new Notification("CondoGames.XYZ",{body:"You will now receive notifications when a new condo gets uploaded!"})
+if("Notification"in window&&Notification.permission=="default"&&getSavedData().notify!=false){setTimeout(()=>swal.fire({title:"Auto Uploads",text:"Do you want to receive notifications when a new condo gets uploaded?",icon:"question",showDenyButton:true,confirmButtonText:'Sure',denyButtonText:'Nah...',}).then((result)=>{if(result.isConfirmed){Notification.requestPermission().then((permission)=>{if(permission!="granted")return;new Notification("CondoGames.XYZ",{body:"You will now receive notifications when a new condo gets uploaded!"})
 setSavedData({notify:true})})}else if(result.isDenied){setSavedData({notify:false})}}),10000)}
